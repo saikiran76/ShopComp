85078d090bce0bfe3b23d7016de3d703ac9a4611
kiran76 <ksknew76@gmail.com>
2024-06-15 02:14:06 +0530

    added hover styles

--- cart/src/components/Item.js
+++ cart/src/components/Item.js
@@ -1,7 +1,7 @@
 const ProductItem = ({ product, addToCart }) => {
     return (
         <div className="product-item border p-2 mt-2 rounded flex flex-col relative h-fit w-[23%] bg-white group">
-            <div className='w-full mb-2'>
+            <div className='w-full mb-2 hover:scale-105 duration-200'>
                 <img className="w-full h-32 object-cover" src={`https://m.media-amazon.com/images/S/aplus-media-library-service-media/c1fce2f7-164d-465a-8955-894bf3d28575.__CR0,23,463,579_PT0_SX362_V1___.jpg`} alt={product.name} />
             </div>
             <h4 className="text-sm font-bold mb-1">{product.name}</h4>
